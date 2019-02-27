f747aa2fe0bbf55d6ae104d8066331c4a2b3c77d
arghasen10 <arghasen10@gmail.com>
2019-02-27 20:33:46 +0530

    New changes added here

--- app/src/main/java/com/logui/arghasen/socketapp/MainActivity.java
+++ app/src/main/java/com/logui/arghasen/socketapp/MainActivity.java
@@ -1,82 +1,79 @@
 package com.logui.arghasen.socketapp;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    public EditText editText_IP,editText_Port,editText_user;
+public class MainActivity extends AppCompatActivity {
+    public EditText editText_IP, editText_Port, editText_user;
     public String ip;
     public int port = 0;
-    public String user;
-    public String finall;
-    public int option;
+    public boolean flag;
     public Socket client;
+    Handler handler = new Handler();
+    String message = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText_IP = (EditText) findViewById(R.id.edittext_IP);
-        editText_Port = (EditText) findViewById(R.id.edittext_Port);
-        editText_user = (EditText) findViewById(R.id.edittext_User);
-        findViewById(R.id.button1).setOnClickListener(this);
-        findViewById(R.id.button2).setOnClickListener(this);
-        findViewById(R.id.button3).setOnClickListener(this);
-        findViewById(R.id.button4).setOnClickListener(this);
-        findViewById(R.id.button5).setOnClickListener(this);
+        editText_IP = findViewById(R.id.edittext_IP);
+        editText_Port = findViewById(R.id.edittext_Port);
 
+        Thread mythread = new Thread(new MyServer());
+        mythread.start();
     }
-    private void sendSocket(){
-        ip = editText_IP.getText().toString();
-        port = Integer.valueOf(editText_Port.getText().toString());
-        user = editText_user.getText().toString();
-        finall = user +","+ String.valueOf(option);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    client = new Socket(ip, port);
-                    PrintWriter printWriter = new PrintWriter(client.getOutputStream());
-                    printWriter.write(finall);
-                    printWriter.flush();
-                    printWriter.close();
-                    client.close();
+
+    class MyServer implements Runnable {
+        ServerSocket ss;
+        Socket mysocket;
+        DataInputStream dis;
+        DataOutputStream dos;
+        PrintWriter pr;
+        @Override
+        public void run() {
+            try {
+                ss = new ServerSocket(9009);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Waiting for sender", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                while (true) {
+                    mysocket = ss.accept();
+                    dos = new DataOutputStream(mysocket.getOutputStream());
+                    pr = new PrintWriter(dos);
+                    pr.write("Hello World");
+                    pr.flush();
+                    pr.close();
+                    dis = new DataInputStream(mysocket.getInputStream());
+                    message = dis.readUTF();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Message Received: " + message, Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
-                catch (Exception e){
-                    e.printStackTrace();
-                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }).start();
-    }
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.button1:
-                option = 1;
-                sendSocket();
-                break;
-            case R.id.button2:
-                option = 2;
-                sendSocket();
-                break;
-            case R.id.button3:
-                option = 3;
-                sendSocket();
-                break;
-            case R.id.button4:
-                option = 4;
-                sendSocket();
-                break;
-            case R.id.button5:
-                option = 5;
-                sendSocket();
-                break;
-
 
         }
+
+
     }
 }
